c8c441f8196e330795e9b8d9edc1f02f5e36ad73
codermarcos <coder.marcos@gmail.com>
2023-01-29 23:50:22 -0300

    :art: improve button login On branch main Changes to be committed: 	modified:   src/components/Header/Header.styles.ts

--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -1,18 +1,29 @@
 import styled from 'styled-components';
 
-import { viewWidth } from 'csx';
+import { border, padding, px, viewWidth } from 'csx';
 
 import Link from 'next/link';
-import { colors } from '@/styles/variables';
+
+import { colors, sizes } from '@/styles/variables';
 
 export const HeaderContainer = styled.header({
   backgroundColor: colors.whiteDefault,
   display: 'flex',
   justifyContent: 'center',
+  padding: padding(sizes.space.default),
   width: viewWidth(100),
 });
 
 export const LoginLink = styled(Link)({
+  backgroundColor: '#d9552d',
+  border: border({
+    color: '#b9573c',
+    style: 'solid',
+    width: sizes.border.xxs,
+  }),
+  borderRadius: sizes.borderRadius.default,
+  color: colors.whiteDefault,
   cursor: 'pointer',
+  padding: padding(sizes.space.default),
   textDecoration: 'none',
 });
