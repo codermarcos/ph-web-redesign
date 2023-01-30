0e31ba80ced17d31f0c10960115f2085c2a28a97
codermarcos <coder.marcos@gmail.com>
2023-01-29 23:50:43 -0300

    :new: add code to client_credentials flow
    This will allow works without login
    On branch main
    Changes to be committed:
    	modified:   src/pages/api/graphql.ts

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,16 +1,40 @@
+/* eslint-disable camelcase */
 import type { NextApiRequest, NextApiResponse } from 'next';
 import fetch from 'isomorphic-fetch';
 
 import cookies from '@/authentication/cookies';
 import jwt from '@/authentication/jwt';
 
+import { accessTokenUri, clientId } from '@/authentication';
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
   const cookie = cookies.retrive(req);
-  const token = jwt.verify(cookie);
+  let token = jwt.verify(cookie);
 
-  if (typeof token === 'string' && token.length === 0)
-    res.send('implement client credentials using lambda cron');
+  if (typeof token !== 'string' || token.length === 0) {
+
+    /*
+     * Here i could just use this if the ph api follows the pattern
+     * const payload = await auth.credentials.getToken();
+     */
+    const response = await fetch(
+      accessTokenUri,
+      {
+        body: JSON.stringify({
+          client_id: clientId,
+          client_secret: process.env.PH_APP_API_SECRET as string,
+          grant_type: 'client_credentials',
+        }),
+        headers: { 'Content-Type': 'application/json' },
+        method: 'post',
+      }
+    );
+    const payload = await response.json();
+
+    token = payload.access_token;
+
+  }
 
   const response = await fetch(`${process.env.PH_API_HOST}v2/api/graphql`,
     {
