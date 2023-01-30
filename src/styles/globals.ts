e7ac58683a5dbd11452189e1e50fe7cc151f65db
codermarcos <coder.marcos@gmail.com>
2023-01-29 22:09:22 -0300

    :art: improve global css On branch main Changes to be committed: 	modified:   src/styles/globals.ts

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -1,11 +1,35 @@
 import { createGlobalStyle } from 'styled-components';
 
-const ResetCss = createGlobalStyle({
+import { percent, px } from 'csx';
+
+import { colors } from '@/styles/variables';
+
+const GlobalsCss = createGlobalStyle({
+  '#__next': {
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+  },
   '*': {
     boxSizing: 'border-box',
     margin: 0,
     padding: 0,
   },
+  'a': {
+    ':visited': {
+      color: 'currentcolor',
+    },
+    textDecoration: 'none',
+  },
+  'body': {
+    backgroundColor: colors.whiteDarkness2,
+    fontFamily: 'Nunito, Segoe UI, Helvetica, Arial',
+  },
+  'main': {
+    maxWidth: px(920),
+    padding: px(20),
+    width: percent(100),
+  },
 });
 
-export default ResetCss;
+export default GlobalsCss;
